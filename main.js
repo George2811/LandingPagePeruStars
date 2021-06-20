ef8e537b48129d4dacb2668fad0c1a1d14fb546f
George <sanchez28112001@gmail.com>
2021-06-19 21:17:36 -0500

    feat: add terms&conditions section

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.mdb.modal', () => {
+    myInput.focus()
+})
