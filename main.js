0ee863dc03df3e067d217653b980a0a09f112760
George <sanchez28112001@gmail.com>
2021-07-01 18:41:43 -0500

    fix: add route to the application

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.mdb.modal', () => {
     myInput.focus()
 })
+
+function goToApp(){
+    window.open("https://peru-stars.web.app/");
+}
